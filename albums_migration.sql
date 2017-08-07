2f1b10646a9f7e339ca2e880f44f9b83bc7c1611
Hunter Warren <hunter.warren@gmail.com>
2017-08-07 15:31:08 -0500

    wheee. databases! first one works!

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,1 +1,13 @@
  USE codeup_test_db;
+
+ DROP TABLE IF EXISTS albums;
+
+ CREATE TABLE IF NOT EXISTS albums (
+ 	id int(10) unsigned NOT NULL AUTO_INCREMENT,
+  	artist varchar(30),
+  	name varchar(30),
+  	release_date YEAR(4),
+  	sales FLOAT(10,2),
+  	genre varchar(22),
+  	PRIMARY KEY (ID)
+  );
