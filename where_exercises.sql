9b5b3d466d256a7ccfdea9a85152b60ea8905fa9
Hunter Warren <hunter.warren@gmail.com>
2017-08-08 14:22:46 -0500

    new file of queries to use.

---
+++ where_exercises.sql
@@ -0,0 +1,19 @@
+/* select emp_no, first_name, last_name
+from employees
+where first_name in ('Irena', 'Vidya', 'Maya'); */
+
+/* select *
+from employees
+where last_name like "e%"; */
+
+/* select *
+from employees
+where hire_date >= 1990-01-01; */
+
+/* select *
+from employees
+where birth_date like "%-12-25"; */
+
+/* select *
+from employees
+where last_name like "%q%"; */
