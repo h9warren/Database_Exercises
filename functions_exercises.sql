c4f340e6e56c0ae7bc5c87767d1b828a5ac930e4
Hunter Warren <hunter.warren@gmail.com>
2017-08-09 11:55:02 -0500

    last function, finds employees with last names containing 'q', but not 'qu'. Then sorts them by group to eliminate dupes, sorted again by last_name asc.

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -15,11 +15,9 @@ from employees
 where hire_date like "1990%"
 and birth_date like "%-12-25";
 
-/* select *
-from employees
-where birth_date like "%-12-25"; */
-
-/* select *
+select first_name, last_name
 from employees
 where last_name like "%q%"
-and last_name not like "%qu%"; 
+and last_name not like "%qu%"
+group by first_name, last_name
+order by last_name;
