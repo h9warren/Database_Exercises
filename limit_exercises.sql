77b6478329ebaf2aa45018fd8400d703b8c99bc7
Hunter Warren <hunter.warren@gmail.com>
2017-08-09 10:33:39 -0500

    sorted to find the 10th group.

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -9,3 +9,10 @@ where hire_date like "199%"
 and birth_date like "%-12-25"
 order by birth_date asc, hire_date desc
 limit 5;
+
+select *
+from employees
+where hire_date like "199%"
+and birth_date like "%-12-25"
+order by birth_date asc, hire_date desc
+limit 5 offset 45;
